847d4b5a905845252275fd11abc1b5056c362af9
Jay Krishnan <jrk26@dhe.duke.edu>
2023-12-14 15:40:47 -0500

    Added commentary regarding work and limited analysis to standard box score statistics

--- PCA.R
+++ PCA.R
@@ -5,28 +5,29 @@ library(MASS)
 library(gbm)
 library(matlab)
 
+#V1: Using traditional count statistics (PTs, RBs, AST, BLK, STL)+exposure (MIN)
+#and Year Drafted
 df<-read.csv('derived_data/clean.csv')
-df_pca<-data.frame(df[4:25])
-df_pca <- as.data.frame(sapply(df_pca, as.numeric))
-df_pca[1:18]<- as.data.frame(sapply(df_pca[1:18], scale))
+df_pca<-data.frame(c(df[4],df[6:7], df[19:22]))
+df_pca<- as.data.frame(sapply(df_pca, as.numeric))
+df_pca<- as.data.frame(sapply(df_pca, scale))
 
 #PCA: Appears that the majority of variance is explained by first 2 PCs
 pca_result<-prcomp(df_pca)
 pca_result
 imagesc(pca_result$x)
 prop_var <- (pca_result$sdev^2) / sum(pca_result$sdev^2)
-prop_var_df<-data.frame(1:22, prop_var)
+prop_var_df<-data.frame(1:7, prop_var)
 colnames(prop_var_df)<-c("PC", "Proportion")
 prop_plot<-ggplot(prop_var_df, aes(x=PC, y=Proportion))+
-      geom_col()+
-      labs(title="Proportion of Variance")
+  geom_col()+
+  labs(title="Proportion of Variance")
+prop_plot
 ggsave("figures/prop_plot.png",plot=prop_plot)
 
-#Plot: REFF has a strong negative correlation along PC1, while the 3P 
-#variables seem to be those weighted most along PC2 (positive association) 
-#based on review of the loadings. Plotting HOF status (final plot) suggests that 
-#there may be a weak association with HOF status and PC1 in particular, in which
-#REFF had the strongest (negative) association.
+#Plot:Plotting HOF status (final plot) suggests that there may be a weak 
+#association with HOF status and PC1 in particular. No great clustering effect 
+#noted, but there seems to be trends that might be exploitable.
 pca_result
 biplot(pca_result)
 PC1 <- pca_result$x[, 1]
@@ -34,6 +35,7 @@ PC2 <- pca_result$x[, 2]
 df_plot<-data.frame(PC1, PC2)
 HOFbiplot<-ggplot(df_plot, aes(PC1, PC2)) +
   geom_point(aes(color=factor(df$HOF)), alpha=0.5)
+HOFbiplot
 
 ggsave("figures/HOFbiplot.png",plot=HOFbiplot)
 
